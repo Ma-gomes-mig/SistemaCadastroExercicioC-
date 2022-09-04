6741ffb354d90e84f6fccb827600b80fc4c32089
Matheus gomes <83517467+Ma-gomes-mig@users.noreply.github.com>
2022-09-04 20:46:19 -0300

    ExercicioSistemaCadastroC#

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCadastro
+{
+    public partial class Cadastrar : Form
+    {
+        List<Pessoa> pessoas;
+
+        public Cadastrar()
+        {
+            InitializeComponent();
+
+            pessoas = new List<Pessoa>();
+
+            comboEC.Items.Add("Casado(a)");
+            comboEC.Items.Add("Solteiro(a)");
+            comboEC.Items.Add("Viuvo(a)");
+            comboEC.Items.Add("Separado(a)");
+
+            comboEC.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                if (pessoa.Nome == txtNome.Text)
+                {
+                    index = pessoas.IndexOf(pessoa);
+                }
+            }
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Preecha o campo Nome.");
+                txtNome.Focus();
+                return;
+            }
+
+            if (txtTelefone.Text == "(  )      -")
+            {
+                MessageBox.Show("Preecha o campo Telefone.");
+                txtTelefone.Focus();
+                return;
+            }
+
+            char sexo;
+
+            if (radioM.Checked)
+            {
+                sexo = 'M';
+            }
+            else 
+            {
+                sexo = 'F';
+            }
+
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.DataNascimento = txtData.Text;
+            p.EstadoCivil = comboEC.SelectedItem.ToString();
+            p.Telefone = txtTelefone.Text;
+            p.CasaPropria = checkCasa.Checked;
+            p.Veiculo = checkVeiculo.Checked;
+            p.Sexo = sexo;
+
+            if (index < 0)
+            {
+                pessoas.Add(p); 
+            }
+            else
+            {
+                pessoas[index] = p;
+            }
+
+            btnLimpar_Click(btnLimpar, EventArgs.Empty);
+
+            Listar();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            int indice = Lista.SelectedIndex;
+            pessoas.RemoveAt(indice);
+            Listar();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtData.Text = "";
+            comboEC.SelectedIndex = 0;
+            txtTelefone.Text = "";
+            checkCasa.Checked = false;
+            checkVeiculo.Checked = false;
+            radioM.Checked = true;
+            radioF.Checked = false;
+            txtNome.Focus();
+        }
+
+        private void Listar()
+        {
+            Lista.Items.Clear();
+            
+            foreach(Pessoa p in pessoas)
+            {
+                Lista.Items.Add(p.Nome);
+            }
+        }
+
+        private void Lista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = Lista.SelectedIndex;
+            Pessoa p = pessoas[indice];
+
+            txtNome.Text = p.Nome;
+            txtData.Text = p.DataNascimento;
+            comboEC.SelectedItem = p.EstadoCivil;
+            txtTelefone.Text = p.Telefone;
+            checkCasa.Checked = p.CasaPropria;
+            checkVeiculo.Checked = p.Veiculo;
+
+            switch (p.Sexo)
+            {                
+                case 'F':
+                    radioF.Checked = true;
+                    break;
+                default :                
+                    radioM.Checked = true;
+                    break;
+                    
+            }
+        }
+    }
+}
